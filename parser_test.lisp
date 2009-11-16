aec90f09c1365d01392c9f5603008f1fb7d0bbfb
Jens Thiede <jens@echo.local>
2009-11-16 11:30:57 +0200

    List parsing works.

--- parser_test.lisp
+++ parser_test.lisp
@@ -5,25 +5,27 @@
         "- two"
         "  - three"
         "    - four"
-        "- five"))
+        "- five"
+        "  - six"
+        "- seven"))
 
-(defparameter *line-one* (make-instance 'line
+(defparameter *list-line-one* (make-instance 'line
                                         :indent-level 0
                                         :content "- one"
                                         :comment "blah"))
-(defparameter *line-two* (make-instance 'line
+(defparameter *list-line-two* (make-instance 'line
                                         :indent-level 0
                                         :content "- two"
                                         :comment ""))
-(defparameter *line-three* (make-instance 'line
+(defparameter *list-line-three* (make-instance 'line
                                           :indent-level 1
                                           :content "- three"
                                           :comment ""))
-(defparameter *line-four* (make-instance 'line
+(defparameter *list-line-four* (make-instance 'line
                                           :indent-level 2
                                           :content "- four"
                                           :comment ""))
-(defparameter *line-five* (make-instance 'line
+(defparameter *list-line-five* (make-instance 'line
                                           :indent-level 0
                                           :content "- five"
                                           :comment ""))
@@ -46,32 +48,32 @@
 
 (def-test-method test-pre-parse ((test parser-test))
   (with-slots (parser) test
-    (assert-lines-equal *line-one* (pre-parse parser "- one # blah"))
-    (assert-lines-equal *line-three* (pre-parse parser "  - three"))
-    (assert-lines-equal *line-four* (pre-parse parser "    - four"))))
+    (assert-lines-equal *list-line-one* (pre-parse parser "- one # blah"))
+    (assert-lines-equal *list-line-three* (pre-parse parser "  - three"))
+    (assert-lines-equal *list-line-four* (pre-parse parser "    - four"))))
 
 (def-test-method test-advance ((test parser-test))
   (with-slots (parser) test
     (assert-lines-equal nil (past-line parser))
-    (assert-lines-equal *line-one* (present-line parser))
-    (assert-lines-equal *line-two* (future-line parser))
+    (assert-lines-equal *list-line-one* (present-line parser))
+    (assert-lines-equal *list-line-two* (future-line parser))
     (advance parser)
-    (assert-lines-equal *line-one* (past-line parser))
-    (assert-lines-equal *line-two* (present-line parser))
-    (assert-lines-equal *line-three* (future-line parser))
+    (assert-lines-equal *list-line-one* (past-line parser))
+    (assert-lines-equal *list-line-two* (present-line parser))
+    (assert-lines-equal *list-line-three* (future-line parser))
     (advance parser)
-    (assert-lines-equal *line-two* (past-line parser))
-    (assert-lines-equal *line-three* (present-line parser))
-    (assert-lines-equal *line-four* (future-line parser))
+    (assert-lines-equal *list-line-two* (past-line parser))
+    (assert-lines-equal *list-line-three* (present-line parser))
+    (assert-lines-equal *list-line-four* (future-line parser))
     (advance parser)
-    (assert-lines-equal *line-three* (past-line parser))
-    (assert-lines-equal *line-four* (present-line parser))
-    (assert-lines-equal *line-five* (future-line parser))))
+    (assert-lines-equal *list-line-three* (past-line parser))
+    (assert-lines-equal *list-line-four* (present-line parser))
+    (assert-lines-equal *list-line-five* (future-line parser))))
 
 
 (def-test-method test-parse-list ((test parser-test))
   (with-slots (parser) test
-    (assert-equal '("one" "two" ("three" ("four")) "five")
+    (assert-equal '("one" "two" ("three" ("four")) "five" ("six") "seven")
                   (parse-list parser))))
 
 (textui-test-run (get-suite parser-test))
